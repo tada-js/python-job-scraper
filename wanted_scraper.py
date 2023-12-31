2a950408d594e667ff6174d643d272131b0f2f58
tada-js <justfix010@gmail.com>
2024-01-01 05:29:54 +0900

    refactor class

--- wanted_scraper.py
+++ wanted_scraper.py
@@ -3,52 +3,68 @@ from bs4 import BeautifulSoup
 import time
 import csv
 
-p = sync_playwright().start()
-browser = p.chromium.launch(headless=False)
-page = browser.new_page()
+class JobScraper:
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.jobs_db = []
 
-page.goto("https://www.wanted.co.kr/jobsfeed")
-time.sleep(2)
-page.click("button.Aside_searchButton__Xhqq3")
-time.sleep(2)
-page.get_by_placeholder("검색어를 입력해 주세요.").fill('프론트엔드')
-time.sleep(2)
-page.keyboard.down("Enter")
-time.sleep(3)
-page.click("a#search_tab_position")
-time.sleep(3)
+    def scrape(self):
+        p = sync_playwright().start()
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
-# 페이지 스크롤
-for x in range(5):
-  page.keyboard.down("End")
-  time.sleep(3)
+        page.goto("https://www.wanted.co.kr/jobsfeed")
+        time.sleep(2)
+        page.click("button.Aside_searchButton__Xhqq3")
+        time.sleep(2)
+        page.get_by_placeholder("검색어를 입력해 주세요.").fill(self.keyword)
+        time.sleep(2)
+        page.keyboard.down("Enter")
+        time.sleep(3)
+        page.click("a#search_tab_position")
+        time.sleep(3)
 
-content = page.content()
-p.stop()
+        # 페이지 스크롤
+        for x in range(5):
+            page.keyboard.down("End")
+            time.sleep(3)
 
-soup = BeautifulSoup(content, "html.parser")
-jobs = soup.find_all("div", class_="JobCard_container__FqChn")
+        content = page.content()
+        p.stop()
 
-jobs_db = []
+        self._parse_content(content)
 
-for job in jobs:
-  link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
-  title = job.find("strong", class_="JobCard_title__ddkwM").text
-  company_name = job.find("span", class_="JobCard_companyName__vZMqJ").text
-  location = job.find("span", class_="JobCard_location__2EOr5").text
-  reward = job.find("span", class_="JobCard_reward__sdyHn").text
-  job = {
-    "title": title,
-    "company_name": company_name,
-    "location": location,
-    "reward": reward,
-    "link": link
-  }
-  jobs_db.append(job)
-  
-file = open("wanted_jobs.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["Title", "Company", "Location", "Reward", "Link"])
+    def _parse_content(self, content):
+        soup = BeautifulSoup(content, "html.parser")
+        jobs = soup.find_all("div", class_="JobCard_container__FqChn")
 
-for job in jobs_db:
-  writer.writerow(job.values())
+        for job in jobs:
+            link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
+            title = job.find("strong", class_="JobCard_title__ddkwM").text
+            company_name = job.find("span", class_="JobCard_companyName__vZMqJ").text
+            location = job.find("span", class_="JobCard_location__2EOr5").text
+            reward = job.find("span", class_="JobCard_reward__sdyHn").text
+            job_info = {
+                "title": title,
+                "company_name": company_name,
+                "location": location,
+                "reward": reward,
+                "link": link
+            }
+            self.jobs_db.append(job_info)
+
+    def save_to_csv(self):
+        file = open(f"{self.keyword}_jobs.csv", "w")
+        writer = csv.writer(file)
+        writer.writerow(["Title", "Company", "Location", "Reward", "Link"])
+
+        for job in self.jobs_db:
+            writer.writerow(job.values())
+
+        file.close()
+
+# scraper = JobScraper('keyword')
+scraper = JobScraper('프론트엔드')
+
+scraper.scrape()
+scraper.save_to_csv()
